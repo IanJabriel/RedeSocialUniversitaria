839292f9a4b8e08e1660969bd84c01ed9c151d6f
João Vitor de Brito Silva <jvbrito72@gmail.com>
2025-03-24 22:28:48 -0300

    Adicionar arquivos de projeto.

---
+++ RedeSocialUniversidade.Domain/Entitys/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedeSocialUniversidade.Domain.Entitys
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Curso { get; set; }
+
+        public List<Usuario>Seguidores { get; set; }
+
+        public Usuario() { 
+            Seguidores = new List<Usuario>();
+        }
+    }
+}
---
+++ RedeSocialUniversidade.Infra/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedeSocialUniversidade.Domain.Entitys;
+
+namespace RedeSocialUniversidade.Infra
+{
+    public class SqlContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=RedeSocialUniversidade");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
+        public DbSet<Evento> Eventos { get; set; }
+
+    }
+}
